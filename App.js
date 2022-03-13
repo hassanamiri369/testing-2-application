d7bdfe7c2737da21bec469a39f53a01f242a1449
Hassanamiri <hassanamiri369@gmail.com>
2022-03-13 14:24:08 +0330

    app flie is changed

---
+++ App.js
@@ -0,0 +1,60 @@
+[33mcommit 48be19504a23d17ea0642bf91105685706426870[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Hassan Amiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 14:14:45 2022 +0330
+
+    moddify input focus
+
+[33mcommit 1d11b659400db78dc6312d5fa621bfe3ed899984[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 14:04:00 2022 +0330
+
+    moddfy
+
+[33mcommit 7a3692613406b6e74421e7b0d63e4b28889ad08c[m
+Merge: f18a538 74d1b3e
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 14:02:40 2022 +0330
+
+    Merge branch 'master' of https://github.com/hassanamiri369/testing-2-application
+
+[33mcommit 74d1b3e5bc31df5b4c49864cfa59a69fc5a418df[m
+Author: Hassan Amiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 14:02:09 2022 +0330
+
+    added focus input
+
+[33mcommit f18a538247c9d43e715855c54f1a261ecab53df9[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 12:52:39 2022 +0330
+
+    added style
+
+[33mcommit 535ac544cee9beeb36bfb8e11369ecebd72ee595[m[33m ([m[1;32maddRemove[m[33m)[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 11:20:21 2022 +0330
+
+    adde remove function todo
+
+[33mcommit f818924b69dc05d5d90fe8ec91b1747c1092a4c0[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 11:16:49 2022 +0330
+
+    added react-id-generator package
+
+[33mcommit 33802fa622e10a7a7a25e629cbe44b07f17e8522[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 11:12:48 2022 +0330
+
+    add todo
+
+[33mcommit 1f4fd0722b60417f24193ee7220ca2e27d7ceb9f[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 10:47:38 2022 +0330
+
+    remove some file
+
+[33mcommit 9978637debfde515068f8d539d2c4507f74cb43e[m
+Author: Hassanamiri <hassanamiri369@gmail.com>
+Date:   Sun Mar 13 10:38:52 2022 +0330
+
+    Initialize project using Create React App
